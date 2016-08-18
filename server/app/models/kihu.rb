39469d55abe3c240478ce6de435007dac83299a5
Tatsuya Kitabatake <e2tod2@gmail.com>
2016-08-18 12:22:15 +0900

    Add Kihu#parse except moves.

--- server/app/models/kihu.rb
+++ server/app/models/kihu.rb
@@ -1,3 +1,13 @@
 class Kihu < ApplicationRecord
   has_many :moves
+
+  def parse (text)
+    parsed = KihuParser.new.parse text
+    self.match_date = parsed[:date]
+    self.rule = parsed[:rule]
+    self.handicap = parsed[:handicap]
+    self.sente = parsed[:sente]
+    self.gote = parsed[:gote]
+    # self.raw_moves = parsed[:moves]
+  end
 end
--- server/spec/models/kihu_spec.rb
+++ server/spec/models/kihu_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Kihu, :type => :model do
-  describe "parse_kihu" do
-
+  describe "parse" do
     it "is common case" do
-      kihu = Kihu.new(
-        teban: 'sente',
-        won: true
-      )
-
-      expect(true).to be true
+      kihu = Kihu.new
+      kihu.parse file_fixture("kihu/kihu1.txt").read
+      expect(kihu.match_date.to_s).to eq '2016-08-02 20:04:56 +0900' 
+      expect(kihu.rule).to eq 'R対局(15分)'
+      expect(kihu.handicap).to eq '平手'
+      expect(kihu.sente).to eq 'sente'
+      expect(kihu.gote).to eq 'gote'
+      # expect(kihu.raw_moves.size).to eq 140
     end
   end
 end
