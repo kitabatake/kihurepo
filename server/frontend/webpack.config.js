2b1954f0d65b01c0326e6160fd9abce405716158
Tatsuya Kitabatake <e2tod2@gmail.com>
2016-08-19 23:58:52 +0900

    適当にwebpackとrailsを連携させてみた。

---
+++ server/frontend/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'kihus/show': './kihus/show.js',
+    vendor: ['react', 'react-dom']
+  },
+  output: { 
+    path: "../app/assets/javascripts/webpack",
+    filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
+  ],
+  devServer: {
+    contentBase: '../'
+  }
+}
