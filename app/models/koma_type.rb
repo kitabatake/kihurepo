79bbaf3c877ff6045ab8595955bd7c63150f0038
Tatsuya Kitabatake <e2tod2@gmail.com>
2016-08-22 20:54:29 +0900

    Add KomaType.

---
+++ app/models/koma_type.rb
@@ -0,0 +1,18 @@
+class Teban < ActiveHash::Base
+  self.data = [
+    {:id => 1, :name => 'hu'},
+    {:id => 2, :name => 'keima'},
+    {:id => 3, :name => 'kyousya'},
+    {:id => 4, :name => 'gin'},
+    {:id => 5, :name => 'kin'},
+    {:id => 6, :name => 'ou'},
+    {:id => 7, :name => 'hisya'},
+    {:id => 8, :name => 'kaku'},
+    {:id => 9, :name => 'to'},
+    {:id => 10, :name => 'nari_kei'},
+    {:id => 11, :name => 'nari_kyou'},
+    {:id => 12, :name => 'nari_gin'},
+    {:id => 13, :name => 'ryuu'},
+    {:id => 14, :name => 'uma'},
+  ]
+end
